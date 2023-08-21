2336f923c7f058803b4716d0e23849b09412d555
jayant pal <jpofficials7@gmail.com>
2023-08-22 00:39:36 +0530

    updates

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,45 @@ const router = express.Router();
 
 //Login Page
 router.get("/login", (req, res) => {
-  res.render('login');
+  res.render("login");
 });
 
 //Register Page
 router.get("/register", (req, res) => {
-    res.render('register');
-  });
+  res.render("register");
+});
+
+//Register Handle
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  //check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
+
+  //Check passwords match
+  if (password !== password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+
+  //Check Pass lengths
+  if (password.length < 6) {
+    errors.push({ msg: "Password should be atleast 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    res.send("pass");
+  }
+});
 
 module.exports = router;

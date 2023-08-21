5683d5704bdca78fdba9f63370f1bc5ed52ae9d4
jayant pal <jpofficials7@gmail.com>
2023-08-22 00:24:04 +0530

    added routes , config, model and views

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://jpofficials7:'+ encodeURIComponent('Jayantjp@786') + '@cluster0.evssxje.mongodb.net/?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
